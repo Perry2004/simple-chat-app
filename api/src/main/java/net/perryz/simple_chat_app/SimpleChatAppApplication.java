8f353023efb8e7e686f8135381e34598de15dd1d
Perry Zhu <perryzhu2004@outlook.com>
2025-07-16 00:38:28 +0000

    chor: setup backend api service

---
+++ api/src/main/java/net/perryz/simple_chat_app/SimpleChatAppApplication.java
@@ -0,0 +1,13 @@
+package net.perryz.simple_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatAppApplication.class, args);
+	}
+
+}
